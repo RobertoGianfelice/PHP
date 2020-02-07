df7407a804fb3ab5d8cf4e14861cc6ce9c3dd145
RobertoGianfelice <rob.gianfelice@gmail.com>
2020-02-07 16:49:53 +0100

    Gestione errore Delete

--- UsoDb/cancellaUtenti.php
+++ UsoDb/cancellaUtenti.php
@@ -1,11 +1,11 @@
 <html>
 <head><title>Registrazione dati</title></head>
 <body>
-<?php 
+<?php
 	include 'vars.php';
 ?>
 <H1> Esecuzione del file registrazione</H1><BR>
-<?php 
+<?php
 	//Apro la sessione php...
 	session_start();
 
@@ -14,26 +14,28 @@
 
 	//Connessione al DB
 	echo "<p>connetto al db...</p>";
-	$link=mysqli_connect( "$db_host", "$db_login","$db_pass") 
-	or die ("Non riesco a connettermi a <b>$db_host"); 
+	$link=mysqli_connect( "$db_host", "$db_login","$db_pass")
+	or die ("Non riesco a connettermi a <b>$db_host");
 
 	//Selezione il DB da utilizzare
-	mysqli_select_db ($link, $database) 
-	or die ("Non riesco a selezionare il db $database<br>"); 
+	mysqli_select_db ($link, $database)
+	or die ("Non riesco a selezionare il db $database<br>");
 
 	//Preparo la Query
 	$dati= " DELETE from UTENTI where cognome='$cognome';";
 	//Eseguo la query
-	mysqli_query ($link, $dati) 
-	or die ("Non riesco ad eseguire la query $dati"); 
-
-	Echo " <CENTER><H1>Eliminato $cognome </H1> </CENTER> ";
-
+	mysqli_query ($link, $dati)
+	or die ("Non riesco ad eseguire la query $dati");
+  if (mysqli_affected_rows($link)>0) {
+		Echo " <CENTER><H1>Eliminato $cognome </H1></CENTER> ";
+  } else {
+		Echo " <CENTER><H1>$cognome non presente nell'elenco degli utenti</H1></CENTER> ";
+	}
 	//Chiudo la connessione
-	mysqli_close ($link); 
-	 
+	mysqli_close ($link);
+
 ?>
 <a href="gestioneUtenti.html">Torna alla pagina home</a>
 
 </body>
-</html> 
+</html>
