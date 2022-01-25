bf5f69c17c72c0b66546213756623ffb222de345
RobertoGianfelice <rob.gianfelice@gmail.com>
2022-01-25 10:58:45 +0100

    aggiornamenti

--- Form/welcome.php
+++ Form/welcome.php
@@ -1,8 +1,8 @@
 <html>
-<body>
+  <body>
 
-Welcome <?php echo "<h1>"$_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+    Welcome <?php echo $_GET["name"]; ?><br>
+    Your email address is: <?php echo $_GET["email"]; ?>
 
-</body>
+  </body>
 </html>
--- Session/credenziali.php
+++ Session/credenziali.php
@@ -8,10 +8,11 @@
   </head>
   <body>
       <?php
+      print("<h2>roberto</h2>");
         if (isset($_SESSION["nome"])) {
           echo " <h1> L'utente attualmente collegato e' " .
                 $_SESSION["nome"] . "</h1>";
-          echo "<a href=\"" . $_SESSION["nome"] . ".html>Vai alla pagina di " . $_SESSION["nome"] ."<\a>";
+          echo "<a href=\"" . $_SESSION["nome"] . ".html\">Vai alla pagina di " . $_SESSION["nome"] ."</a>";
         } else{
           echo "<h1>Attualmente non c'e' nessun utente collegato </h1>";
         }
