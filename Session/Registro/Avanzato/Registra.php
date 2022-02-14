1bca2e47669ddd2175716da4f71f0014dc1a6213
RobertoGianfelice <rob.gianfelice@gmail.com>
2022-02-14 16:40:48 +0100

    Correzioni varie e semplificazione inserimento nel registro

--- Session/Registro/Avanzato/Registra.php
+++ Session/Registro/Avanzato/Registra.php
@@ -9,27 +9,15 @@
       $materia = $_GET["materia"];
       $voto = $_GET["voto"];
 
-      if (!isset($_SESSION["Voti"])) {
-        // Il campo voti non esiste: lo creo come un array
-        $_SESSION["Voti"]=array();
-      }
-
       if (!isset($_SESSION["Voti"][$nome])) {
         // Il campo $nome non esiste all'interno di voti: lo creo
-        $_SESSION["Voti"][$nome] = array();
-        echo "Aggiungo il voto in " . $materia . "<br";
+        echo "Aggiungo il voto in " . $materia . "<br>";
       } else {
-        echo "Aggiorno il voto in " . $materia . "<br";
+        echo "Aggiorno il voto in " . $materia . "<br>";
       }
 
-      // Recupero l'array associato a $nome all'interno di Voti in Session
-      $votazione = $_SESSION["Voti"][$nome];
-      // Inserisco o aggiorno la votazione
-      $votazione[$materia] = $voto;
-      // Inserisco la nuova votazione nella struttura voti relativa a $nome
-      $_SESSION["Voti"][$nome]=$votazione;
-      echo "Aggiungo il voto in " . $materia . "<br";
-
+      $_SESSION["Voti"][$nome][$materia]=$voto;
+      echo "<br>Voto Inserito nel registro di ". $nome . "<br>";
       print_r($_SESSION);
     ?>
     <br>
