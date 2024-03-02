726c4678a0b84c2766c47b802cd7d5228edd8dae
RobertoGianfelice <rob.gianfelice@gmail.com>
2024-03-02 18:23:49 +0100

    Piccola Correzione

--- UsoDb/CaricaImmagini/removeImg.php
+++ UsoDb/CaricaImmagini/removeImg.php
@@ -14,6 +14,7 @@
     ?>
     <h1>Cancellazione Immagine</h1>
     <?php
+    
     if (isset($_GET["toBeRemoved"]) and $_GET["toBeRemoved"] != "") {
         $fileToRemove = $_GET["toBeRemoved"];
         echo "<p>connetto al db...</p>";
