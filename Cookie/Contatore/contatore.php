f20b3417c21bb38167401ec79f34f209da2da366
RobertoGianfelice <rob.gianfelice@gmail.com>
2024-12-03 08:08:37 +0100

    Cookie: Aggiunto esercizio Contatore

---
+++ Cookie/Contatore/contatore.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrazione</title>
+</head>
+
+<body>
+    <?php
+    //Recupero parametri di ingresso
+    if (isset($_REQUEST["incremento"]) && $_REQUEST["incremento"]!="") {
+        $incremento=$_REQUEST["incremento"];
+    } else {
+        $incremento=0;
+    }
+
+    if (isset($_COOKIE["contatore"])) {
+        $cookie_contatore = $_COOKIE["contatore"];
+    }else {
+        $cookie_contatore=0;
+    }
+    $contatore_aggiornato=$cookie_contatore+$incremento;
+    switch ($_REQUEST["Bottone"]) {
+        case "Aggiungi":
+            echo"uno";
+            setcookie("contatore", $contatore_aggiornato, time() + 60, "/");
+            echo "<h2>Contatore vale " . $contatore_aggiornato . "</h2>";
+        break;
+        case "Azzera":
+            echo"due";
+            setcookie("contatore", 0, time() + 60, "/");
+            echo "<h2>Contatore azzerato</h2>";
+        break;
+        default:
+            echo "Valore ricevuto " . $_REQUEST["Bottone"];
+            break;
+        
+    }
+
+    ?>
+    <h3>Aggiornamento effettuato</h3>
+    <a href="contatore.html" >Home</a>
+</body>
+
+</html>
--- Cookie/Login/registrazione.php
+++ Cookie/Login/registrazione.php
@@ -9,15 +9,22 @@
 
 <body>
     <?php
-    //Memorizzo i dati sul cookie "utente"
+    //Recupero parametri di ingresso
+    if ((!isset($_REQUEST["login"]) or $_REQUEST["login"] == "") or
+        (!isset($_REQUEST["password"]) or $_REQUEST["password"] == "")) {
+        echo "<h1>Parametri di login mancanti</h1>";
+        echo "<a href='registrazione.html'>Registrati</a>";
+        exit();
+    }
+
     $cookie_login = $_REQUEST["login"];
     $cookie_password = $_REQUEST["password"];
 
-    //Cookie valido per 30 secondi
-    setcookie("login", $cookie_login, time() + 1800, "/");
-    setcookie("password", $cookie_password, time() + 1800, "/");
+    //Cookie valido per 60 secondi
+    setcookie("login", $cookie_login, time() + 60, "/");
+    setcookie("password", $cookie_password, time() + 60, "/");
     ?>
-    <h3>Accesso effettuato: hai 30 secondi per proseguire</h3>
+    <h3>Registrazione effettuata</h3>
     <a href="Welcome.php" target="_blank">Usa il Servizio</a>
 </body>
 
